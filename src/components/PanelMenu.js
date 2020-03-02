230a3d9d6ea8128c49b2ca0dd8729bb00f0ee749
Marian Marcu <marianmarcu58@gmail.com>
2020-03-02 20:42:15 +0200

    Add logout item, css for login, register

--- src/components/PanelMenu.js
+++ src/components/PanelMenu.js
@@ -31,6 +31,10 @@ class PAnelMenu extends Component {
                    label:'Events',
                    icon:'pi pi-fw pi-calendar',
                    url:'/events'
+                },
+                {  label: 'Sign Out', 
+                   icon: 'pi pi-fw pi-power-off',
+                   url: '/login'
                 }
             ]
         };
