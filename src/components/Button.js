6999368a1b993f625fd0e0d57817bfd542476ab1
Marian Vasi <marian.vasi@el2.de>
2020-03-03 10:47:27 +0200

    Added section dinamicaly

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,11 @@ import { Button } from 'primereact/button';
 class ButtonSubmit extends Component {
 
     render() {
-        const { label, type} = this.props
+        const { label, type, onClick, icon} = this.props
         return (
             <div>
                 <div className="content-button">
-                    <Button type={type} label={label}/>
+                    <Button type={type} icon={icon} label={label} onClick={onClick}/>
                 </div>
             </div>
         )
--- src/container/dashboard/DashboardPage/Dashboard.js
+++ src/container/dashboard/DashboardPage/Dashboard.js
@@ -13,6 +13,9 @@ import Input from 'components/Input';
 import dashboard from 'reducers/index';
 import { dashboardActions } from 'container/dashboard/actions/dashboard.actions';
 
+const Item = ()=> {
+    
+}
 
 class Dashboard extends Component {  
 
@@ -20,14 +23,14 @@ class Dashboard extends Component {
         super(props);
 
         this.state = {
-            dash: {
-                projectName: null,
-                arrivalTime: null,
-                departureTime: null,
+            dash: [{
+                projectName: "",
+                arrivalTime: "",
+                departureTime: "",
                 pause: "00:30",
-                date: null,
-                comment: null
-            },
+                date: "",
+                comment: "",
+            }],
              submitted: false 
         };
         this.handleChange = this.handleChange.bind(this);
@@ -61,6 +64,37 @@ class Dashboard extends Component {
         }
     }
 
+    // addSection = (e) => {
+    //     this.setState((prevState) => ({
+    //         dash: [
+    //             ...prevState.dash,
+    //             {
+    //                 projectName: null,
+    //                 arrivalTime: null,
+    //                 departureTime: null,
+    //                 pause: "00:30",
+    //                 comment: null,
+    //             }
+    //         ]
+    //     }));
+    // }
+
+    addInputs = () => {
+        const item = {
+            projectName: null,
+            arrivalTime: null,
+            departureTime: null,
+            pause: "00:30",
+            comment: null
+        }
+        let newDash = [...this.state.dash, item]
+        this.setState({
+            dash: newDash
+        })
+        // var newInputs = this.state.dash.length;
+        // this.setState(prevState => ({dash: prevState.dash.concat([newInputs]) }));
+    }
+
     render() {
         const { projects, dashboardPost } = this.props; 
         const { dash, submitted } =  this.state;
@@ -74,28 +108,46 @@ class Dashboard extends Component {
                                 <div className="calendar">
                                     <CAlendar id="calendar" name="date" value={dash.date} placeholder="Date" onChange={this.handleChange}/>
                                 </div>
-                                <div className="dropdown">
-                                    {projects.pending && <em>Loading users...</em>}
-                                    {projects.error && <span className="text-danger">ERROR: {projects.error}</span>}
-                                    <Dropdown id="dropdown" name="projectName" value={dash.projectName} onChange={this.handleChange} options={projects.items} />
+                                <div className="addButton">
+                                    <Button type="button" icon="pi pi-plus" onClick={ this.addInputs } />
                                 </div>
-                                <div className="timeSection">
-                                    <div className="arrivalTime">
-                                        <InputTime id="arrivalTime" name="arrivalTime" value={dash.arrivalTime} placeholder="Arrival time" onChange={this.handleChange} />
-                                    </div>
-                                    <div className="departureTime">
-                                        <InputTime id="departureTime" name="departureTime" value={dash.departureTime} placeholder="Departure time" onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="pause">
-                                        <Input type="time" id="pause" name="pause" value={dash.pause} placeholder="Pause" onChange={this.handleChange} />
-                                    </div>
-                                    <div className="textarea">
-                                        <INputTextarea id="textArea" name="textArea" value={dash.comment} placeholder="Comment" onChange={this.handleChange}/>
-                                    </div>
-                                    <div className="dash-save-button">
-                                        <Button type="submit" label="SAVE" />
-                                        { dashboardPost }
-                                    </div>
+                                {
+                                   dash.map((item, index) => {
+                                        return(
+                                            <div className="dynamicContent" key={index}>
+                                                <div>
+                                                    <span>
+                                                    <div className="deleteButton">
+                                                        <Button type="button" icon="pi pi-times" />
+                                                    </div>
+                                                    <div className="dropdown">
+                                                        {projects.pending && <em>Loading users...</em>}
+                                                        {projects.error && <span className="text-danger">ERROR: {projects.error}</span>}
+                                                        <Dropdown id="dropdown" name="projectName" value={item.projectName} onChange={this.handleChange} options={projects.items} />
+                                                    </div>
+                                                    </span>
+                                                </div>
+                                                <div className="timeSection">
+                                                    <div className="arrivalTime">
+                                                        <InputTime id="arrivalTime" name={`arrivalTime${index}`} value={item.arrivalTime} placeholder="Arrival time" onChange={this.handleChange} />
+                                                    </div>
+                                                    <div className="departureTime">
+                                                        <InputTime id="departureTime" name={`departureTime${index}`} value={item.departureTime} placeholder="Departure time" onChange={this.handleChange}/>
+                                                    </div>
+                                                    <div className="pause">
+                                                        <Input type="time" id="pause" name={`pause${index}`} value={item.pause} placeholder="Pause" onChange={this.handleChange} />
+                                                    </div>
+                                                    <div className="textarea">
+                                                        <INputTextarea id="textArea" name={`textArea${index}`} value={item.comment} placeholder="Comment" onChange={this.handleChange}/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                                <div className="dash-save-button">
+                                    <Button type="submit" label="SAVE" />
+                                    { dashboardPost }
                                 </div>
                             </div>
                         </div>
