6e7a3da824172c94b7dd2e3f4d574f7026e002bd
Marian Vasi <marian.vasi@el2.de>
2020-03-09 12:43:16 +0200

    Used final form for login, added api modul

--- src/container/login/LoginPage/Login.js
+++ src/container/login/LoginPage/Login.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Button from 'components/Button';
-import Input from 'components/Input';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import authentication from 'reducers';
 
 import { userActions } from '../actions/user.actions';
 import 'static/login.css';
+import { Form } from 'react-final-form';
+import LoginForm from './LoginForm';
 
 class Login extends Component {
 
@@ -22,25 +21,16 @@ class Login extends Component {
             password: '',
             submitted: false
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange = (e) => {
-        // this.setState({
-        //     [e.target.id]: e.target.value 
-        // })
-
         const { name, value } = e.target;
         this.setState({ [name]: value });
     }
 
-    handleSubmit = (e)=> {
-        e.preventDefault();
-        console.log("LOgin state: ",this.state);
-        // let { email, password } = this.state
-        // this.props.login(email, password);
+    handleSubmit = (e) => {
+        // e.preventDefault();
+        console.log("LOgin state: ", this.state);
 
         this.setState({ submitted: true });
         const { email, password } = this.state;
@@ -49,44 +39,21 @@ class Login extends Component {
         }
 
     }
-    
+
     render() {
 
         // let { isLoginPending, isLoginSuccess, loginError} = this.props
 
-        const { loggingIn } =  this.state;
+        const { loggingIn } = this.state;
         const { email, password, submitted } = this.state;
 
         return (
-            <div className='containerLogin'>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <h3>Sign in</h3>
-                    <div className={'form-group' + (submitted && !email ? ' has-error' : '' )} >
-                        <Input type="email" id="email" className="form-control"  name="email"  value={email} placeholder="Email Address*" onChange={this.handleChange} />
-                        {
-                            submitted && !email &&
-                            <div className="help-block"> Email is required </div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <Input type="password" id="password" className="form-control" name="password" value={password} placeholder="Password*" onChange={this.handleChange} />
-                        {
-                            submitted && !password && 
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-
-                    <div className="form-group">
-                        <Button type="submit" label= "SIGN IN"/>
-
-                        <p className="linkToRegister"> Don't have an account? <Link to="register">Sign Up</Link></p>
-                    </div>
-
-                    {loggingIn} 
-
-                </form>
-                
-            </div>
+            <Form
+                onSubmit={this.handleSubmit}
+                render={props => (
+                    <LoginForm {...{ submitted, password, email, loggingIn, ...props }} handleChange={this.handleChange}/>
+                )}
+            />
         );
     }
 }
@@ -101,4 +68,4 @@ const actionCreators = {
     logout: userActions.logout
 };;
 
-export default connect(mapState, actionCreators) (Login);
+export default connect(mapState, actionCreators)(Login);
---
+++ src/container/login/LoginPage/LoginForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from 'components/Button';
+import Input from 'components/Input';
+import { Link } from 'react-router-dom';
+import 'static/login.css';
+
+const LoginForm = ({ handleSubmit, handleChange, submitted, password, loggingIn, email }) => (
+    <div className='containerLogin'>
+        <form name="form" onSubmit={handleSubmit}>
+            <h3>Sign in</h3>
+            <div className={'form-group' + (submitted && !email ? ' has-error' : '')} >
+                <Input type="email"
+                    id="email"
+                    className="form-control"
+                    name="email"
+                    value={email}
+                    placeholder="Email Address*"
+                    onChange={handleChange}
+
+                />
+                {
+                    submitted && !email &&
+                    <div className="help-block"> Email is required </div>
+                }
+            </div>
+            <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                <Input type="password"
+                    id="password"
+                    className="form-control"
+                    name="password"
+                    value={password}
+                    placeholder="Password*"
+                    onChange={handleChange}
+                />
+                {
+                    submitted && !password &&
+                    <div className="help-block">Password is required</div>
+                }
+            </div>
+
+            <div className="form-group">
+                <Button type="submit" label="SIGN IN" />
+
+                <p className="linkToRegister"> Don't have an account? <Link to="register">Sign Up</Link></p>
+            </div>
+
+            {loggingIn}
+
+        </form>
+
+    </div>
+);
+
+
+export default LoginForm;
