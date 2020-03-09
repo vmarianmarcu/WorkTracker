040204e65c625e5d50b641d5640aaee3411bc797
Marian Vasi <marian.vasi@el2.de>
2020-03-09 16:11:15 +0200

    DashboardForm Added

---
+++ src/container/dashboard/DashboardForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import INputTextarea from 'components/InputTextarea';
+import Dropdown from 'components/Dropdown';
+import InputTime from 'components/InputTime24H';
+import 'static/dashboard.css';
+import Input from 'components/Input';
+
+const Item = ({ item, projects, handleChange, index }) => (
+    <div className="dynamicContent">
+        <div className="dropdownAndPause">
+            <span>
+                <div className="dropdown">
+                    {projects.pending && <em>Loading users...</em>}
+                    {projects.error && <span className="text-danger">ERROR: {projects.error}</span>}
+                    <Dropdown id="dropdown" name={"projectName" + index} value={item.projectName} onChange={handleChange} options={projects.items} />
+                </div>
+            </span>
+            <span>
+                <div className="pause">
+                    <Input type="time" id="pause" name={"pause" + index} value={item.pause} placeholder="Pause" onChange={handleChange} />
+                </div>
+            </span>
+        </div>
+
+        <div className="timeSection">
+            <span>
+                <div className="arrivalTime">
+                    <InputTime id="arrivalTime" name={"arrivalTime" + index} value={item.arrivalTime} placeholder="Arrival time" onChange={handleChange} />
+                </div>
+            </span>
+            <span>
+                <div className="departureTime">
+                    <InputTime id="departureTime" name={"departureTime" + index} value={item.departureTime} placeholder="Departure time" onChange={handleChange} />
+                </div>
+            </span>
+        </div>
+        <div className="textarea">
+            <span>
+                <INputTextarea id="textArea" name={"textArea" + index} value={item.comment} placeholder="Comment" onChange={handleChange} />
+            </span>
+        </div>
+    </div>
+)
+
+export default Item;
--- src/container/login/RegisterPage/PegisterForm.js
+++ src/container/login/RegisterPage/PegisterForm.js
@@ -4,43 +4,70 @@ import Button from 'components/Button';
 import { Link } from 'react-router-dom';
 import 'static/register.css';
 
-const RegisterForm = ({submitted, user, registering, handleSubmit, handleChange}) => (
-            <div className='containerRegister'>
-                <h3>Sign up</h3>
-                <form onSubmit={handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
-                        <Input type="text" id="firstName" className="form-control" name="firstName" value={user.firstName} placeholder="FirstName*" onChange={handleChange} />
-                        {
-                            submitted && !user.firstName && 
-                            <div className="help-block">First Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
-                        <Input type="text" id="lastName" className="form-control" name="lastName" value={user.lastName} placeholder="LastName*" onChange={handleChange} />
-                        {submitted && !user.lastName && 
-                            <div className="help-block">Last Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
-                        <Input type="email" id="email" className="form-control" name="email" value={user.email} placeholder="Email Address*" onChange={handleChange} />
-                        {submitted && !user.email &&
-                            <div className="help-block">Email is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-                        <Input type="password" id="password" className="form-control" name="password" value={user.password} placeholder="Password*" onChange={handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <Button type="submit" label="SIGN UP" />
-                        { registering }
-
-                        <p className="linkToRegister"> Already have an account? <Link to="login">Sign in</Link></p>
-                    </div>
-                </form>
-                
+const RegisterForm = ({ submitted, user, registering, handleSubmit, handleChange }) => (
+    <div className='containerRegister'>
+        <h3>Sign up</h3>
+        <form onSubmit={handleSubmit}>
+            <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
+                <Input type="text"
+                    id="firstName"
+                    className="form-control"
+                    name="firstName"
+                    value={user.firstName}
+                    placeholder="FirstName*"
+                    onChange={handleChange}
+                />
+                {
+                    submitted && !user.firstName &&
+                    <div className="help-block">First Name is required</div>
+                }
             </div>
+            <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
+                <Input type="text"
+                    id="lastName"
+                    className="form-control"
+                    name="lastName"
+                    value={user.lastName}
+                    placeholder="LastName*"
+                    onChange={handleChange}
+                />
+                {submitted && !user.lastName &&
+                    <div className="help-block">Last Name is required</div>
+                }
+            </div>
+            <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+                <Input type="email"
+                    id="email"
+                    className="form-control"
+                    name="email"
+                    value={user.email}
+                    placeholder="Email Address*"
+                    onChange={handleChange}
+                />
+                {submitted && !user.email &&
+                    <div className="help-block">Email is required</div>
+                }
+            </div>
+            <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                <Input type="password"
+                    id="password"
+                    className="form-control"
+                    name="password"
+                    value={user.password}
+                    placeholder="Password*"
+                    onChange={handleChange} />
+                {submitted && !user.password &&
+                    <div className="help-block">Password is required</div>
+                }
+            </div>
+            <div className="form-group">
+                <Button type="submit" label="SIGN UP" />
+                {registering}
+
+                <p className="linkToRegister"> Already have an account? <Link to="login">Sign in</Link></p>
+            </div>
+        </form>
+
+    </div>
 )
 export default RegisterForm;
