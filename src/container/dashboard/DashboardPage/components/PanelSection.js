ed262853fa15e66a5a68682d525772e718a85450
Marian Vasi <marian.vasi@el2.de>
2020-04-01 01:20:08 +0300

    Changed name of DashboardForm to Items

--- src/container/dashboard/DashboardPage/components/PanelSection.js
+++ src/container/dashboard/DashboardPage/components/PanelSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Panel } from 'primereact/panel';
-import Item from './DashboardForm';
+import Items from './Items';
 import DeleteItem from './DeleteItem';
 
 const PanelSection = ({ name, projects, dash, onClick }) => (
@@ -10,7 +10,7 @@ const PanelSection = ({ name, projects, dash, onClick }) => (
                 <div className="panel">
                     <div>
                         <Panel header={`Project ${index + 1}`} toggleable={true}>
-                            <Item name={name} key={index} index={index} item={item} projects={projects} />
+                            <Items name={name} key={index} index={index} item={item} projects={projects} />
                             <DeleteItem onClick={onClick} />
                             <br />
                             <br />
