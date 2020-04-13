0918b47b753f338d1d8e76f872885129a149ade0
Marian Vasi <marian.vasi@el2.de>
2020-04-13 18:27:12 +0300

    ApiMiddleware added

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,15 +1,17 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
+import { apiMiddleware } from 'api';
 
 const loggerMiddleware = createLogger();
 
 const store = createStore(
     rootReducer,
-        applyMiddleware(
-            thunkMiddleware,
-            loggerMiddleware
-    ), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware,
+        apiMiddleware
+    )
 );
 export default store;
