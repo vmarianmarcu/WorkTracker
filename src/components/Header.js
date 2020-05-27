87f8edef501b1d883bcf2f6dc8e2dfd79e361165
Marian Vasi <marian.vasi@el2.de>
2020-05-27 19:10:55 +0300

    Header component  modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Header = ({ title }) => (<header>{title}</header>);
+const Hr = () => (<hr />);
 
 const HeaderComponent = () => (
+    <div>
     <Header title="" />
+    <Hr  />
+    </div>
 )
 export default HeaderComponent;
