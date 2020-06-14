1b220ff81fe84c70ee8208382210253e42c44943
Marian Vasi <marian.vasi@el2.de>
2020-06-14 13:33:21 +0300

    Old get projects action deleted

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 
 import { combineReducers } from 'redux';
 import { authentication } from 'reducers/authentication.reducer';
-import { projects } from 'reducers/project.reducer';
 import {reducers as DataReducers} from 'data/reducers'
 
 const rootReducer = combineReducers({
    authentication,
-   projects,
    ...DataReducers
 });
 
