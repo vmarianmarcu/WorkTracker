2015d9d08ed36885b5ba8731e3cce091c6e91d12
Marian Vasi <marian.vasi@el2.de>
2020-10-27 15:38:11 +0200

    Id param for button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
 import { Button } from 'primereact/button';
 
-const ButtonSubmit = ({ className, label, type, onClick, icon, disabled }) => (
+const ButtonSubmit = ({ id, className, label, type, onClick, icon, disabled }) => (
     <Fragment>
         <Button
+            id={id}
             className={className}
             type={type}
             icon={icon}
